0c51579397639d0807dbc49bb795794b5a388613
I Wayan Rupadana <rupadanawayan@gmail.com>
2023-09-09 01:12:47 +0800

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     domains: ['picsum.photos', 'res.cloudinary.com', 'admin.codecrafters.id'],
 
   },
+  experimental: {
+    appDir: true
+  }
 };
 
 module.exports = nextConfig;
