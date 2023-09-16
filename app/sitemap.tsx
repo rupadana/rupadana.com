ab94954ba1d304f739485eacb6f9b0cda5fa6955
I Wayan Rupadana <rupadanawayan@gmail.com>
2023-09-16 18:33:21 +0800

    Update sitemap

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -2,6 +2,8 @@ import { MetadataRoute } from 'next'
 
 import domain from '@/common/libs/domain';
 import getLearnSeries from './learn/data';
+import { ContentProps } from '@/common/types/learn';
+import axios from 'axios';
 
 
 
@@ -9,6 +11,38 @@ import getLearnSeries from './learn/data';
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
     const learnSeries = await getLearnSeries();
+
+    let domains = [
+        ...learnSeries.map(item => {
+            return {
+                url: domain(`/learn/${item.slug}`),
+                lastModified: item.updated_at as string
+            }
+        })
+    ]
+
+    await Promise.all([
+        ...learnSeries.map(async (series: ContentProps) => {
+            return new Promise(async resolve => {
+                const endpoint = process.env.CMS_API_URL + '/learn-series/' + series.slug + '/contents';
+                const contents = await axios.get(endpoint) as any;
+
+                await Promise.all([
+                    contents.data.data.subContents.map((item: any) => {
+                        return new Promise(resolve => {
+                            domains.push({
+                                url: domain(`/learn/${series.slug}/${item.slug}`),
+                                lastModified: item.updated_at as string
+                            })
+                            resolve(domains)
+                        })
+                    })
+                ])
+
+                resolve(domains)
+            })
+        })
+    ])
 
     return [
         {
@@ -35,12 +69,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             url: domain('/roadmap'),
             lastModified: new Date(),
         },
+        ...domains
 
-        ...learnSeries.map(item => {
-            return {
-                url: domain(`/${item.slug}`),
-                lastModified: item.updated_at as string
-            }
-        })
     ]
 }
