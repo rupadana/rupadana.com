c939e49ffb3048a69eedc6594f962ec85c640e79
I Wayan Rupadana <rupadanawayan@gmail.com>
2023-09-30 21:33:18 +0800

    add projects

--- common/constant/projects.ts
+++ common/constant/projects.ts
@@ -10,7 +10,21 @@ export const PROJECT_CONTENTS: ContentProps[] = [
     link_demo: 'https://github.com/rupadana/filament-api-service',
     is_show: true,
     link_github: 'https://github.com/rupadana/filament-api-service',
-    stacks: '["php"]',
+    stacks: '["PHP", "Laravel"]',
+    updated_at: '2023-09-09',
+    content: "",
+    is_featured: true
+  },
+  {
+    id: 2,
+    title: 'rupadana.com',
+    slug: 'personal-website',
+    description: 'Personal website built with Next.js',
+    image: 'https://res.cloudinary.com/rupadana/image/upload/v1696080088/Screenshot_2023-09-30_at_21.17.35_ovr93h.png',
+    link_demo: 'https://rupadana.com',
+    is_show: true,
+    link_github: 'https://github.com/rupadana/rupadana.com',
+    stacks: '["Next.js", "TypeScript", "TailwindCSS"]',
     updated_at: '2023-09-09',
     content: "",
     is_featured: true
